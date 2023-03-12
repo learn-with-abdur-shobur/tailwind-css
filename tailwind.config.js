5046801301f1dc49ac48fb6eef5de99d05e1f0b9
abdur-shobur <abdurshobur.developer@gmail.com>
2023-03-12 23:47:56 +0600

    multiple theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ module.exports = {
 				skin: {
 					wh: 'var(--text-color-wh)',
 					dr: 'var(--text-color-dark)',
+					head: 'var(--text-color-head)',
+					para: 'var(--text-color-para)',
 				},
 			},
 			backgroundColor: {
 				skin: {
 					hs: 'var(--bg-color-has)',
+					p: 'var(--bg-color-p)',
 				},
 			},
 		},
